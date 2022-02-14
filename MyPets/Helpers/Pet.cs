49061a531880154de35b9e180eeaa1ef9a28bb0e
towernter <towernter@gmail.com>
2022-02-14 23:09:14 +0200

    Added a delete, add and display functionality

---
+++ MyPets/Helpers/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPets.Helpers
+{
+    public class Pet
+    {
+        public int id { get; set; }
+        public string dog_name { get; set; }
+        public int breed_id { get; set; }
+
+        public int age { get; set; }
+    }
+}
